689d8bc454a9851cb87f78ecc525cd989443794f
mazacon <mazacon@protonmail.com>
2022-10-03 06:49:10 -0500

    Add apod_json command/code clean up

--- src/bot.py
+++ src/bot.py
@@ -16,47 +16,46 @@ bot = lightbulb.BotApp(
 
 @bot.listen(hikari.StartedEvent)
 async def on_started(event):
-    # will want to read all old jobs and restart them
-    # for lines in csv, for guild, add_job(apod, 'cron',) add_job(launch)
-    print('Bot has started!')
+    # todo: restart jobs in store
+    print('Lightyear has started!')
 
 @bot.command
-@lightbulb.command('ly', 'Lightyear')
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def ly(context):
-    pass
+@lightbulb.option('date', 'Date in YYYY-MM-DD format(Defaults to today)', required=False)
+@lightbulb.command('apod', 'Get embed APOD card')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def apod_get(context):
+    await context.respond(embed_apod(context.options.date))
 
-@ly.child
-@lightbulb.command('help', 'Show Lightyear options')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def formatted_help(context):
-    await context.respond('Later, Ill be of help')
+@bot.command
+@lightbulb.option('date', 'Date in YYYY-MM-DD format(Defaults to today)', required=False)
+@lightbulb.command('apod_json', 'Retrieve raw JSON data for an APOD')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def apod_json(context):
+    pretty_json = json.dumps(apod_http_request(context.options.date), indent=2)
+    await context.respond(f'```{pretty_json}```')
 
-
-# TODO: Extract these commmands out to extension folder
-
-@ly.child
+@bot.command
 @lightbulb.option('time', 'Time in HH:MM format (24hr CST)')
 @lightbulb.option('channel_id', 'ID of channel to schedule APODs')
-@lightbulb.command('apod_set', 'Add APOD job to specified channel.')
-@lightbulb.implements(lightbulb.SlashSubCommand)
+@lightbulb.command('apod_set', 'Add scheduled APOD job to specified channel.')
+@lightbulb.implements(lightbulb.SlashCommand)
 async def schedule_embed_apod(context):
     time = context.options.time.split(':')
     hour = time[0]
     minute = time[1]
 
-    scheduler.add_job(send_embed_apod, trigger='cron', args=[context.options.channel_id], hour=hour, minute=minute)
+    scheduler.add_job(
+        send_embed_apod,
+        trigger='cron',
+        args=[context.options.channel_id],
+        hour=hour,
+        minute=minute,
+        id=f'apod_{context.options.channel_id}'
+    )
     
     await context.respond(
         f'APOD job added to {context.options.channel_id} daily at {context.options.time}.'
     )
-
-@ly.child
-@lightbulb.option('date', 'Date in YYYY-MM-DD format(Defaults to today)', required=False)
-@lightbulb.command('apod_get', 'Pass in date in MM/DD/YYYY format')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def apod_get(context):
-    await context.respond(embed_apod(context.options.date))
 
 async def send_embed_apod(channel_id):
     await bot.rest.create_message(channel_id, embed=embed_apod())
@@ -69,6 +68,7 @@ def embed_apod(date=None):
         color = hikari.Color(0xff2e65)
     )
     embed.set_image(apod_json['url'])
+    embed.set_footer(f"APOD for {apod_json['date']}")
     
     return embed
 
@@ -80,8 +80,8 @@ def apod_http_request(date):
     print(apod_url)
     response = requests.get(apod_url)
     json_response = json.loads(response.text)
+    print(json_response)
 
     return json_response
 
-# bot.load_extensions_from('./extensions')
 bot.run()
