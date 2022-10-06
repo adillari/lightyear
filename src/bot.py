b0a90a67f4b598d446390ae12872f019ba471165
mazacon <mazacon@protonmail.com>
2022-10-06 03:58:24 -0500

    Implement job removal/display

--- src/bot.py
+++ src/bot.py
@@ -7,13 +7,10 @@ from hikari.colors import Color
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.mongodb import MongoDBJobStore
 
-scheduler = AsyncIOScheduler(jobstores={'mongo': MongoDBJobStore()})
-
 bot = lightbulb.BotApp(
     token = constants.DISCORD_BOT_TOKEN,
     default_enabled_guilds = constants.ENABLED_GUILDS
 )
-
 scheduler = AsyncIOScheduler(jobstores={'mongo': MongoDBJobStore()})
 
 @bot.listen(hikari.StartedEvent)
@@ -25,49 +22,66 @@ async def on_started(event):
 @lightbulb.option('date', 'Date in YYYY-MM-DD format(Defaults to today)', required=False)
 @lightbulb.command('apod', 'Get embed APOD card')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def apod_get(context):
-    pending = await context.respond('Fetching your APOD!')
-    await send_embed_apod(context.channel_id, context.options.date)
+async def apod_get(ctx):
+    pending = await ctx.respond('Fetching your APOD!')
+    await send_embed_apod(ctx.channel_id, ctx.options.date)
     await pending.delete()
 
 @bot.command
 @lightbulb.option('date', 'Date in YYYY-MM-DD format(Defaults to today)', required=False)
 @lightbulb.command('apod_json', 'Retrieve raw JSON data for an APOD')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def apod_json(context):
-    pretty_json = json.dumps(apod_http_request(context.options.date), indent=2)
-    await context.respond(f'```{pretty_json}```')
+async def apod_json(ctx):
+    pretty_json = json.dumps(apod_http_request(ctx.options.date), indent=2)
+    await ctx.respond(f'```{pretty_json}```')
+
+@bot.command
+@lightbulb.option('channel_id', 'Channel to remove job from')
+@lightbulb.command('apod_remove', 'Remove scheduled APOD job')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def apod_remove(ctx):
+    job_id = f'{ctx.guild_id}:{ctx.options.channel_id}'
+
+    if scheduler.get_job(job_id):
+        scheduler.remove_job(job_id)
+        await ctx.respond('Job removed successfully')
+    else:
+        await ctx.respond('Invalid channel id')
+
+
+@bot.command
+@lightbulb.command('show_jobs', 'Show running APOD jobs')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def show_jobs(ctx):
+    jobs = scheduler.get_jobs(jobstore='mongo')
+    await ctx.respond(jobs)
 
 @bot.command
 @lightbulb.option('time', 'Time in HH:MM format (24hr CST)')
 @lightbulb.option('channel_id', 'ID of channel to schedule APODs')
 @lightbulb.command('apod_set', 'Add scheduled APOD job to specified channel.')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def schedule_embed_apod(context):
-    time = context.options.time.split(':')
+async def schedule_embed_apod(ctx):
+    time = ctx.options.time.split(':')
     hour = time[0]
     minute = time[1]
 
-    job_id = f'{context.guild_id}:{context.options.channel_id}'
+    job_id = f'{ctx.guild_id}:{ctx.options.channel_id}'
     if scheduler.get_job(job_id):
         scheduler.remove_job(job_id)
         
     scheduler.add_job(
         send_embed_apod,
         trigger='cron',
-        args=[context.options.channel_id],
+        args=[ctx.options.channel_id],
         hour=hour,
         minute=minute,
         jobstore='mongo',
         id=job_id
     )
-
-    scheduler.print_jobs(jobstore='mongo')
-    jobs = scheduler.get_jobs(jobstore='mongo')
-    print(jobs)
     
-    await context.respond(
-        f'APOD job added to {context.options.channel_id} daily at {context.options.time}.'
+    await ctx.respond(
+        f'APOD job added to {ctx.options.channel_id} daily at {ctx.options.time}.'
     )
  
 async def send_embed_apod(channel_id, date=None):
